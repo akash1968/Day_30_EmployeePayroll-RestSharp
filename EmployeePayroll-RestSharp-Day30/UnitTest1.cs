eb9241d5fd021e168ce95f04f61316a9e73b2885
akash1968 <akash.singh670@gmail.com>
2020-11-17 00:23:33 +0530

    [Akash] .Added UC5-DeleteEmployeeFromJsonServer Employee Payroll RestSharp

--- EmployeePayroll-RestSharp-Day30/UnitTest1.cs
+++ EmployeePayroll-RestSharp-Day30/UnitTest1.cs
@@ -137,5 +137,22 @@ namespace EmployeePayroll_RestSharp_Day30
             Assert.AreEqual("95000", employee.Salary);
             Console.WriteLine(response.Content);
         }
+        /// <summary>
+        /// UC 5 : Delete the employee details with given id
+        /// </summary>
+        [TestMethod]
+        public void OnCallingDeleteAPI_ReturnSuccessStatus()
+        {
+            //Arrange
+            //Initialize the request for PUT to add new employee
+            RestRequest request = new RestRequest("/Employees/7", Method.DELETE);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Console.WriteLine(response.Content);
+        }
     }
 }
