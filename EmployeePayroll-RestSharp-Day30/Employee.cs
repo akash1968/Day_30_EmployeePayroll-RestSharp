16f5697e06f59f16dacaa9056e102ea82f89e22f
akash1968 <akash.singh670@gmail.com>
2020-11-16 23:52:46 +0530

    [Akash] .Added  Employee Payroll RestSharp

---
+++ EmployeePayroll-RestSharp-Day30/Employee.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Employee.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akash Kumar Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayroll_RestSharp_Day30
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+}
